9618943cb162ed57f1a4d06d580e84a070564b5d
Redox <mail@redox.to>
2024-11-09 00:16:41 +0100

    Add pregiveweapons hook

--- lua/custom_loadout/sv_main.lua
+++ lua/custom_loadout/sv_main.lua
@@ -42,6 +42,7 @@ function CLoadout:GiveWeapons( ply )
     local maxPrimary = GetConVar( "custom_loadout_primary_limit" ):GetInt()
     local maxSecondary = GetConVar( "custom_loadout_secondary_limit" ):GetInt()
 
+    hook.Run( "CLoadoutPreGiveWeapons", ply, items )
     local preferredWeapon = "weapon_physgun"
 
     for _, item in ipairs( items ) do
